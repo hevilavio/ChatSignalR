542c8b63792052fa977f93bee1861c6177a860e2
Hevilavio Soares <hevilavio.soares@hotmail.com.br>
2014-04-23 14:58:24 -0300

    Cópia local de todas as dll referenciadas no projeto

--- Chat/ChatSignalR/DAO/DdosDAO.cs
+++ Chat/ChatSignalR/DAO/DdosDAO.cs
@@ -2,42 +2,42 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
-using System.Web;
-using MySql.Data.MySqlClient;
+using System.Web; 
 
 namespace ChatSignalR.DAO
 {
     public class DdosDAO
     {
-        string _urlConexao;
-        public DdosDAO()
-        {
-            _urlConexao = ConfigurationManager.ConnectionStrings["ChatConnection"].ToString();
-        }
+    //    string _urlConexao;
+    //    public DdosDAO()
+    //    {
+    //        _urlConexao = ConfigurationManager.ConnectionStrings["ChatConnection"].ToString();
+    //    }
 
         public long InserirDados(string dados)
         {
-            long id = 0;
-            using (MySqlConnection conn = new MySqlConnection(_urlConexao))
-            {
-                MySqlCommand cmd = conn.CreateCommand();
-                cmd.CommandType = System.Data.CommandType.StoredProcedure;
-                cmd.CommandText = "spInserirDadoDDOS";
+    //        long id = 0;
+    //        using (MySqlConnection conn = new MySqlConnection(_urlConexao))
+    //        {
+    //            MySqlCommand cmd = conn.CreateCommand();
+    //            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+    //            cmd.CommandText = "spInserirDadoDDOS";
 
-                MySqlParameter p1 = new MySqlParameter("_dados", MySqlDbType.VarChar);
-                p1.Value = dados;
+    //            MySqlParameter p1 = new MySqlParameter("_dados", MySqlDbType.VarChar);
+    //            p1.Value = dados;
 
 
-                cmd.Parameters.Add(p1);
+    //            cmd.Parameters.Add(p1);
                 
-                cmd.Connection.Open();
-                cmd.ExecuteNonQuery();
+    //            cmd.Connection.Open();
+    //            cmd.ExecuteNonQuery();
 
-                id = cmd.LastInsertedId;
+    //            id = cmd.LastInsertedId;
 
-            }
-
-            return id;
+    //        }
+            
+    //      return id;
+            return 15;
         }
     }
 }
--- Chat/ChatSignalR/DAO/Hubs/ChatDAO.cs
+++ Chat/ChatSignalR/DAO/Hubs/ChatDAO.cs
@@ -2,51 +2,50 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
-using System.Web;
-using MySql.Data.MySqlClient;
+using System.Web; 
 
 namespace ChatSignalR.DAO.Hubs
 {
-    public class ChatDAO
-    {
-        private string _urlConexao;
+    //public class ChatDAO
+    //{
+    //    private string _urlConexao;
 
-        public ChatDAO()
-        {
-            _urlConexao = ConfigurationManager.ConnectionStrings["ChatConnection"].ToString();
-        }
+    //    public ChatDAO()
+    //    {
+    //        _urlConexao = ConfigurationManager.ConnectionStrings["ChatConnection"].ToString();
+    //    }
 
-        public long AdicionarMensagem(string remetente, string mensagem, DateTime dataHora)
-        {
-            long id = 0;
-            using (MySqlConnection conn = new MySqlConnection(_urlConexao))
-            {
-                MySqlCommand cmd = conn.CreateCommand();
-                cmd.CommandType = System.Data.CommandType.StoredProcedure;
-                cmd.CommandText = "spInserirMensagem";
+    //    public long AdicionarMensagem(string remetente, string mensagem, DateTime dataHora)
+    //    {
+    //        long id = 0;
+    //        using (MySqlConnection conn = new MySqlConnection(_urlConexao))
+    //        {
+    //            MySqlCommand cmd = conn.CreateCommand();
+    //            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+    //            cmd.CommandText = "spInserirMensagem";
 
-                MySqlParameter p1 = new MySqlParameter("_remetente", MySqlDbType.VarChar);
-                p1.Value = remetente;
+    //            MySqlParameter p1 = new MySqlParameter("_remetente", MySqlDbType.VarChar);
+    //            p1.Value = remetente;
 
-                MySqlParameter p2 = new MySqlParameter("_mensagem", MySqlDbType.VarChar);
-                p2.Value = mensagem;
+    //            MySqlParameter p2 = new MySqlParameter("_mensagem", MySqlDbType.VarChar);
+    //            p2.Value = mensagem;
 
-                MySqlParameter p3 = new MySqlParameter("_dtHora", MySqlDbType.DateTime);
-                p3.Value = dataHora;
+    //            MySqlParameter p3 = new MySqlParameter("_dtHora", MySqlDbType.DateTime);
+    //            p3.Value = dataHora;
 
 
-                cmd.Parameters.Add(p1);
-                cmd.Parameters.Add(p2);
-                cmd.Parameters.Add(p3);
+    //            cmd.Parameters.Add(p1);
+    //            cmd.Parameters.Add(p2);
+    //            cmd.Parameters.Add(p3);
 
-                cmd.Connection.Open();
-                cmd.ExecuteNonQuery();
+    //            cmd.Connection.Open();
+    //            cmd.ExecuteNonQuery();
 
-                id = cmd.LastInsertedId;
+    //            id = cmd.LastInsertedId;
 
-            }
+    //        }
 
-            return id;
-        }
-    }
+    //        return id;
+    //    }
+    //}
 }
