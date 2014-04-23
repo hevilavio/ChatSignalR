90c5d0d90d4b7ee59f79933db15d1a5bd2a287bf
Hevilavio Soares <hevilavio.soares@hotmail.com.br>
2014-04-23 16:04:37 -0300

    DLL do MySql e ajustes em algumas classes.

--- Chat/ChatSignalR/DAO/DdosDAO.cs
+++ Chat/ChatSignalR/DAO/DdosDAO.cs
@@ -2,42 +2,42 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
-using System.Web; 
+using System.Web;
+using MySql.Data.MySqlClient; 
 
 namespace ChatSignalR.DAO
 {
     public class DdosDAO
     {
-    //    string _urlConexao;
-    //    public DdosDAO()
-    //    {
-    //        _urlConexao = ConfigurationManager.ConnectionStrings["ChatConnection"].ToString();
-    //    }
+        string _urlConexao;
+        public DdosDAO()
+        {
+            _urlConexao = ConfigurationManager.ConnectionStrings["ChatConnection"].ToString();
+        }
 
         public long InserirDados(string dados)
         {
-    //        long id = 0;
-    //        using (MySqlConnection conn = new MySqlConnection(_urlConexao))
-    //        {
-    //            MySqlCommand cmd = conn.CreateCommand();
-    //            cmd.CommandType = System.Data.CommandType.StoredProcedure;
-    //            cmd.CommandText = "spInserirDadoDDOS";
+            long id = 0;
+            using (MySqlConnection conn = new MySqlConnection(_urlConexao))
+            {
+                MySqlCommand cmd = conn.CreateCommand();
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.CommandText = "spInserirDadoDDOS";
 
-    //            MySqlParameter p1 = new MySqlParameter("_dados", MySqlDbType.VarChar);
-    //            p1.Value = dados;
+                MySqlParameter p1 = new MySqlParameter("_dados", MySqlDbType.VarChar);
+                p1.Value = dados;
 
 
-    //            cmd.Parameters.Add(p1);
-                
-    //            cmd.Connection.Open();
-    //            cmd.ExecuteNonQuery();
+                cmd.Parameters.Add(p1);
 
-    //            id = cmd.LastInsertedId;
+                cmd.Connection.Open();
+                cmd.ExecuteNonQuery();
 
-    //        }
-            
-    //      return id;
-            return 15;
+                id = cmd.LastInsertedId;
+
+            }
+
+            return id;
         }
     }
 }
